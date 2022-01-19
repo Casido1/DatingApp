22286590ecf2ad2d322dcc22cfa3815da8bc8f74
Casido1 <ugochukwu.anunihu@gmail.com>
2022-01-19 09:20:11 +0100

    Initial

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _cntx;
+        public UsersController(DataContext cntx)
+        {
+            _cntx = cntx;
+            
+        }
+
+        [HttpGet("")]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _cntx.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers([FromRoute] int id)
+        {
+            return _cntx.Users.Find(id);
+             
+        }
+    }
+}
